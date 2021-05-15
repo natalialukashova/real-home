f95641b6734278ce230f34c18afc90a3981ba334
Наталья Лукашова <n.lukashowa@gmail.com>
2021-05-15 15:23:47 +0300

    added offering for the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="home">
     <Carousel />
+    <div class="container home__offering">
+      <div class="row">
+        <div class="col-md-4  home_border">
+          <h3 class="home__offering-title">We are Offering the Best Estate Deals</h3>
+        </div>
+        <div class="col-md-8">
+          <p class="home__paragraph">
+            Quisque diam lorem interdum vitaapibus ac scelerisque vitae pede.
+            Donec eget tellus non erat lacinia fertum. Donec in velit vel ipsum
+            auctovinar. Proin umcorper urna et felisstibulum iaculis lacinia
+            est. Proin dictum elem entum velit fusce euismod. Aenean commodo
+            ligula eget dolor. Aenean massa. Lorem ipsum dolor sit amet, consec
+            tetuer adipis elit, aliquam eget nibh etlibura. Lorem ipsum dolor
+            sitamet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Carousel from '../components/Carousel'
+import Carousel from "../components/Carousel";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Carousel,
-  }
-}
+  },
+};
 </script>
